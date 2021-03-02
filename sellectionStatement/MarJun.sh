03f6d99b79bc03a5fff39969b58b164f1cbce688
Kartikkavi <78947510+Kartikkavi@users.noreply.github.com>
2021-03-01 21:50:56 -0800

    Update MarJun.sh

--- sellectionStatement/MarJun.sh
+++ sellectionStatement/MarJun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-read -p "Enter the date"date
-read -p "Enter the month"month
-
-if (( ($month<=6&&$date<=20)&&($month>=3&$date>=20)&&($date<31) ))
+read -p "Enter the date" date
+read -p "Enter the month" month
+echo $date
+if (( ($month<=6 && $date<=20) && ($month>=3 && $date>=20) && ($date<31) ))
 then
 echo $month $date "true"
 else
